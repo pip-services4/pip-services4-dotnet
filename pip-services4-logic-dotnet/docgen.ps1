93377b21e3ae5250fedc5900ccd2a92881da9cae
Dmitriy Krayniy <dkrayniy@entinco.com>
2023-10-30 16:28:43 +0400

    Added pipelines and updated devops files

--- pip-services4-logic-dotnet/docgen.ps1
+++ pip-services4-logic-dotnet/docgen.ps1
@@ -5,26 +5,26 @@ $ErrorActionPreference = "Stop"
 
 # Generate image and container names using the data in the "component.json" file
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
-
-$docImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
-$container=$component.name
+$docsImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
+$container = $component.name
 
 # Remove build files
-if (Test-Path "./docs") {
-    Remove-Item -Recurse -Force -Path "./docs/*"
-} else {
-    New-Item -ItemType Directory -Force -Path "./docs"
+if (Test-Path "$PSScriptRoot/docs") {
+    Remove-Item -Recurse -Force -Path "$PSScriptRoot/docs"
+}
+else {
+    New-Item -ItemType Directory -Force -Path "$PSScriptRoot/docs"
 }
 
 # Build docker image
-docker build -f docker/Dockerfile.docgen -t $docImage .
+docker build -f "$PSScriptRoot/docker/Dockerfile.docs" -t $docsImage $PSScriptRoot
 
 # Create and copy compiled files, then destroy the container
-docker create --name $container $docImage
-docker cp "$($container):/app/docs/." ./docs
+docker create --name $container $docsImage
+docker cp "$($container):/app/docs/html/." "$PSScriptRoot/docs"
 docker rm $container
 
-if (!(Test-Path "./docs")) {
-    Write-Host "docs folder doesn't exist in root dir. Build failed. Watch logs above."
-    exit 1
+# Verify that docs folder was indeed created after generating documentation
+if (-not (Test-Path "$PSScriptRoot/docs")) {
+    Write-Error "docs folder doesn't exist in root dir. Build failed. See logs above for more information."
 }
